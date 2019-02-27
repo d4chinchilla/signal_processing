482bd3f5d340532c75e2942a5c74130e513a88f9
mjohns4 <33690293+mjohns4@users.noreply.github.com>
2019-02-27 19:53:48 +0000

    Works on F303RE at 50kHz

--- arm_main.cpp
+++ arm_main.cpp
@@ -1,35 +1,28 @@
 #include "mbed.h"
-#include <queue>
-#include <vector>
 
-#define ADC0_PIN D9
-#define ADC1_PIN D10
-#define ADC2_PIN D11
-#define ADC3_PIN D12
-#define CS_PIN D8
-#define CLK_PIN D7
+#define ADC0_PIN PA_0
+#define ADC1_PIN PA_1
+#define ADC2_PIN PA_3
+#define ADC3_PIN PA_4
+#define CS_PIN D5
+#define CLK_PIN D6
 
-#define BUFFER_SIZE 2048   // If this goes above 255 then some variables may need to be expanded
+#define BUFFER_SIZE 10
 #define DATA_BITS 9
 #define NUM_MICS 4
-#define CLK_DELAY 3
+#define CLK_DELAY 8
 #define BAUD 1500000
-#define START_BYTE 0XFF
+#define START_BYTE 0xFF
 DigitalOut cs(CS_PIN);
-DigitalOut clk(D7);
-//BusIn adcs(D9, D10, D11, D12);
-RawSerial serial(USBTX, USBRX);
+DigitalOut clk(CLK_PIN);
+Serial serial(USBTX, USBRX);
 
-//int sample_buffer[BUFFER_SIZE];
-//char serial_buffer[ 1+ 7*BUFFER_SIZE ]; // Includes 4 chars for sending samples, and a start packet
 char serial_buffer[NUM_MICS*BUFFER_SIZE];
-//int current_sample[4];
-//vector<int> current_sample;
 uint16_t current_sample[NUM_MICS];
 uint16_t samples_buffer[BUFFER_SIZE*NUM_MICS];
-//queue< vector<int> > samples_buffer;
 uint16_t top = 0;
 const char start = 0x30;
+uint8_t stall=0;
 
 DigitalIn a0(D9);
 DigitalIn a1(D10);
@@ -47,44 +40,39 @@ void read_samples()
         // Bit-bashing the ADCs as if SPI
         // The data only starts on the third falling edge, so put two clocks outside the loop
         cs = 0;
-        //wait_us(1);
+        // wait_us(1);
+//         for(uint8_t d=0; d <= CLK_DELAY; d++) { __asm volatile("NOP"); }
         clk = 1;
-//        wait_us(1);
-        for(uint8_t d=0; d <= CLK_DELAY; d++) {__nop();}
+// // #pragma GCC push_options
+// // #pragma GCC optimze ("no-unroll-loops")
+        // for(uint8_t d=0; d <= CLK_DELAY; d++) { __asm volatile("NOP"); }
+//         // wait_us(1);
         clk = 0;
-//        wait_us(1);
-        for(uint8_t d=0; d <= CLK_DELAY; d++) {__nop();}
-        clk = 1;
-//        wait_us(1);
-        for(uint8_t d=0; d <= CLK_DELAY; d++) {__nop();}
-        clk = 0;
-        
+// #pragma GCC pop_options
         for(uint8_t i = 0; i < DATA_BITS; i++)
         {
             clk = 1;
-//            wait_us(1);
-            for(uint8_t d=0; d <= CLK_DELAY-1; d++) {__nop();}
+#pragma GCC push_options
+#pragma GCC optimze ("no-unroll-loops")
+            for(uint8_t d=0; d <= CLK_DELAY; d++) { stall=d; __asm volatile("NOP"); }
+            // wait_us(1);
+#pragma GCC pop_options
             clk = 0;
-//            temp = adcs.read();
-            //a0.read();
-////            a1.read();
-////            a2.read();
-////            a3.read();
-            
+
             current_sample[0] += a0 << (DATA_BITS-(i+1));
             current_sample[1] += a1 << (DATA_BITS-(i+1));
             current_sample[2] += a2 << (DATA_BITS-(i+1));
             current_sample[3] += a3 << (DATA_BITS-(i+1));
             
+            // wait_us(1);
             // Debateable need for a delay in here to maintain clock pulse - hoping
             // the above logic is slow enough to generate a delay of about 1us anyway
         }
-        
         // The MSB is a sign bit, and should always be 0. If it isn't, the bit
         // may have been corrupted and the sample should be set to 0.
         for(uint8_t i = 0; i < NUM_MICS; i++)
         {
-            if(current_sample[i] >> DATA_BITS-1)
+            if(current_sample[i] >> (DATA_BITS-1))
                 current_sample[i] = 0;
         }
         
@@ -98,15 +86,16 @@ void send_serial()
 //    serial_buffer[1] = 0x34;
 //    serial.printf("Sending serial\n");
 
-//    serial.putc(START_BYTE);
+    // serial.putc(START_BYTE);
+    while(!serial.writeable()){}
     serial.putc(0x30);
     for(uint16_t i = 0; i < BUFFER_SIZE*NUM_MICS; i++)
     {
 //        serial.printf("serial loop\n");
         serial_buffer[i] = (uint8_t)(samples_buffer[i] >> 4);
+        // while(!serial.writeable()){}
         serial.putc(serial_buffer[i]);
     }
-    
 //    serial.printf(&start);
 //    serial.printf(serial_buffer);
     top = 0;
@@ -118,6 +107,7 @@ int main()
     clk = 0;
     
     serial.baud(BAUD);    // THIS NEEDS TO BE SET THE SAME ON THE PI
+    // serial.set_blocking(true);
     
 //    serial.printf("Started\n");
     
